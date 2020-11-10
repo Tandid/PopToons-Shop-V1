9c909b1538a7d51703eeefaa48c9461866ae98c8
Tandid Alam <tandid.alam@gmail.com>
2020-11-09 22:04:43 -0500

    Added MUI to Nav

--- src/client/components/Navbar.js
+++ src/client/components/Navbar.js
@@ -1,23 +1,42 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
-class Navbar extends React.Component {
-    constructor() {
-     super()
-    }
+const useStyles = makeStyles((theme) => ({
+  title: {
+    margin: 0
+    },
 
-    render() {
-        return (
-            <div>
-                <nav>
-                <NavLink to="/"> Home Page </NavLink>
-                <NavLink to="/products"> Products </NavLink>
-                <NavLink to="/cart"> Cart # </NavLink>
-                <NavLink to="/orders"> Order History </NavLink>
-                </nav>
-            </div>
-        )
-    }
+}));
+
+ 
+const Navbar = () => {
+    const classes = useStyles();
+
+    return (
+        <div>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography className={classes.title} variant="h6">
+                        <NavLink to="/"> Funko Pop Shop </NavLink>
+                    </Typography>
+                    <Typography className={classes.title} variant="h6">
+                        <NavLink to="/products"> Products </NavLink>
+                    </Typography>
+                    <Typography className={classes.title} variant="h6">
+                        <NavLink to="/cart"> Cart # </NavLink>
+                    </Typography>
+                    <Typography className={classes.title} variant="h6">
+                        <NavLink to="/orders"> Order History </NavLink>
+                    </Typography>
+                </Toolbar>
+            </AppBar>    
+        </div>
+    )
+    
 }
 
 export default Navbar
