00fa155239137ad0600c1c419dcb4845626c823c
Tandid Alam <tandid.alam@gmail.com>
2020-11-10 20:45:27 -0500

    added button to banner

--- src/client/components/HomePage.js
+++ src/client/components/HomePage.js
@@ -4,6 +4,8 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
+import {ThemeProvider} from '@material-ui/styles'
+import theme from '../theme'
 
 import Footer from './Footer';
 
@@ -35,6 +37,12 @@ const useStyles = makeStyles((theme) => ({
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover',
       },
+      button: {
+        position: 'absolute',
+        height: theme.spacing(7),
+        width: theme.spacing(26),
+        top: '47%'
+      }
   }));
 
 
@@ -43,10 +51,13 @@ const HomePage = () => {
     const classes = useStyles();
 
         return (
-            <div >
+            <ThemeProvider theme={theme}>
             <CssBaseline />
             <Grid container justify="center" className={classes.root}>
             <Paper className={classes.banner} style={{backgroundImage: 'url(https://www.bemoregeek.co.uk/content/images/carousels/bmg-web-banners5-9927.jpg)'}}/>
+            <Button className={classes.button} variant="contained" color="primary">
+            SHOP NOW
+            </Button>
             <Grid container justify="center">
             <Paper className={classes.largePost} style={{backgroundImage: 'url(https://cdn10.bigcommerce.com/s-6r10lz/product_images/uploaded_images/pop-dbz-banner.jpg?t=1488346408)'}}/>
             <Paper className={classes.miniPost} style={{backgroundImage: 'url(https://www.tierragamer.com/wp-content/uploads/2020/09/Dragon-Ball-Z-Nuevos-Funko-Pop-01.jpg)'}}/>
@@ -58,7 +69,7 @@ const HomePage = () => {
             <Paper className={classes.banner} style={{backgroundImage: 'url(https://cdn2.bigcommerce.com/server500/d90a4/product_images/theme_images/funkopop-sf-banner__89066.jpg?t=1482074497)'}}/>
             </Grid>
             <Footer title="Contact" description="Check out my portfolio here!" />
-          </div>
+            </ThemeProvider>
 
         )
     
