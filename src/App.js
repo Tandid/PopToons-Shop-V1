cd5ab0f4c127fb67aa186a31596a067153342c98
Tandid Alam <tandid.alam@gmail.com>
2020-11-16 23:05:42 -0500

    fixed minor error

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import {Navbar} from "./client/components/index"
-import Routes from "./routes"
+import React from "react";
+import { Navbar } from "./client/components/index";
+import Routes from "./routes";
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <Routes/>
+      <Navbar />
+      <Routes />
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 // import {Provider} from 'react-redux'
-import App from './app';
+import App from "./app";
 
 ReactDOM.render(
   // <Provider>
-    <Router>
-      <App />
-    </Router>
+  <Router>
+    <App />
+  </Router>,
   // </Provider>
-  ,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
