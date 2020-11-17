822a633078909e4692589bea86aa067048112b61
Tandid Alam <tandid.alam@gmail.com>
2020-11-16 22:10:03 -0500

    added dropdown menu bar from mui

--- src/client/components/Navbar.js
+++ src/client/components/Navbar.js
@@ -1,44 +1,124 @@
-import React from 'react'
-import {Link, NavLink} from 'react-router-dom'
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
 import Tab from "@material-ui/core/Tab";
-import {ThemeProvider} from '@material-ui/styles'
-import theme from '../theme'
+import { ThemeProvider } from "@material-ui/styles";
+import theme from "../theme";
 
-
+import Button from "@material-ui/core/Button";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import Grow from "@material-ui/core/Grow";
+import Paper from "@material-ui/core/Paper";
+import Popper from "@material-ui/core/Popper";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuList from "@material-ui/core/MenuList";
 
 const useStyles = makeStyles((theme) => ({
   title: {
-    margin: 0
-    },
-
+    margin: 0,
+  },
+  root: {
+    display: "flex",
+  },
+  paper: {
+    marginRight: theme.spacing(2),
+  },
 }));
 
- 
 const Navbar = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <ThemeProvider theme={theme}>
-                      <CssBaseline/>
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
 
-            <AppBar style={{ margin: 0 }} position="static">
-                <Toolbar>
-                    <Typography className={classes.title} variant="h6">
-                        <NavLink to="/"> Funko Pop Shop </NavLink>
-                    </Typography>
-                    <Tab label="Products" component={Link} to="products" />
-                    <Tab label="Cart #" component={Link} to="/cart" />
-                    <Tab label="Order History" component={Link} to="/orders" />
-                </Toolbar>
-            </AppBar>    
-        </ThemeProvider>
-    )
-    
-}
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
 
-export default Navbar
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  function handleListKeyDown(event) {
+    if (event.key === "Tab") {
+      event.preventDefault();
+      setOpen(false);
+    }
+  }
+
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+
+      <AppBar style={{ margin: 0 }} position="static">
+        <Toolbar>
+          <Grid container justify="space-between">
+            <Tab label="Funko Pop Shop" component={Link} to="/" />
+            <Grid justify="space-around">
+              <Tab label="Products" component={Link} to="products" />
+              <Tab label="Cart #" component={Link} to="/cart" />
+              <Tab label="Order History" component={Link} to="/orders" />
+              <Tab
+                ref={anchorRef}
+                aria-controls={open ? "menu-list-grow" : undefined}
+                aria-haspopup="true"
+                onClick={handleToggle}
+                label="Hello, Guest"
+              />
+              <Popper
+                open={open}
+                anchorEl={anchorRef.current}
+                role={undefined}
+                transition
+                disablePortal
+              >
+                {({ TransitionProps, placement }) => (
+                  <Grow
+                    {...TransitionProps}
+                    style={{
+                      transformOrigin:
+                        placement === "bottom" ? "center top" : "center bottom",
+                    }}
+                  >
+                    <Paper>
+                      <ClickAwayListener onClickAway={handleClose}>
+                        <MenuList
+                          autoFocusItem={open}
+                          onKeyDown={handleListKeyDown}
+                        >
+                          <MenuItem onClick={handleClose}>Profile</MenuItem>
+                          <MenuItem onClick={handleClose}>My account</MenuItem>
+                          <MenuItem onClick={handleClose}>Logout</MenuItem>
+                        </MenuList>
+                      </ClickAwayListener>
+                    </Paper>
+                  </Grow>
+                )}
+              </Popper>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </ThemeProvider>
+  );
+};
+
+export default Navbar;
