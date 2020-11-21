4e63a566999957fb225fde34cc666b8477fb8453
Tandid Alam <tandid.alam@gmail.com>
2020-11-21 01:48:56 -0500

    deleted old auth form

--- src/client/components/index.js
+++ src/client/components/index.js
@@ -4,6 +4,5 @@ export { default as Cart } from "./Cart";
 export { default as Orders } from "./Orders";
 export { default as HomePage } from "./HomePage";
 export { default as ProductDetails } from "./productDetails";
-export { Login, Signup } from "./auth-form";
 export { default as LoginForm } from "./Login.js";
 export { default as SignupForm } from "./Signup.js";
