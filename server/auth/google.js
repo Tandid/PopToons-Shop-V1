613ceaa70e404cbbb06aa6d126dc917502ea73f9
Tandid Alam <tandid.alam@gmail.com>
2020-12-08 20:09:52 -0500

    got rid of some files. test on heroku now

--- server/auth/google.js
+++ server/auth/google.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
 const { User } = require("../db/models");
 module.exports = router;
-const keys = require("./keys.json");
+// const keys = require("./keys.json");
 
 /**
  * For OAuth keys and other secrets, your Node process will search
